df8b48f34ebcfdda4587a8bbbfbc3ab18932538f
zboyz <sramin_m@ymail.com>
2020-12-21 17:19:54 +0330

    Update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
       exec_mode: 'fork',
       port: 3000,
       instances: '1', // Or a number of instances
-      script: './node_modules/nuxt/bin/nuxt.js  --hostname 127.0.0.1 --port 3000',
+      script: './node_modules/nuxt/bin/nuxt.js',
       args: 'start',
       watch: true
     }
