1fe5df008b160fe278b77c2bbf55ca1bec5f22d9
zboyz <sramin_m@ymail.com>
2020-12-21 17:12:18 +0330

    Update

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,7 +29,7 @@ export default {
   server: {
     port: 3000, // default: 3000
     host: '127.0.0.1' // default: localhost
-  }
+  },
   /*
    ** Headers of the page
    ** See https://nuxtjs.org/api/configuration-head
