5fd72e12a8bf78f438510ca3addf7d3e23485b87
zboyz <sramin_m@ymail.com>
2020-12-21 16:54:03 +0330

    Update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,9 +2,8 @@ module.exports = {
   apps: [
     {
       name: 'Upera',
-      exec_mode: 'cluster',
+      exec_mode: 'fork',
       port: 3000,
-      host: 'next.upera.tv',
       instances: '10', // Or a number of instances
       script: './node_modules/nuxt/bin/nuxt.js',
       args: 'start'
