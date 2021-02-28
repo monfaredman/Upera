a1edfc4fafd3d3d4ebce17148151f5c49424cd30
Ramin Mousavi <raminmousavi@Ramins-MBP.lan>
2021-03-01 00:33:36 +0330

    update

--- pages/namad.vue
+++ pages/namad.vue
@@ -4,7 +4,7 @@
       <div>
 <a referrerpolicy="origin" target="_blank" href="https://trustseal.enamad.ir/?id=204904&amp;Code=ipcUNmaUZGludD970sgm"><img referrerpolicy="origin" src="https://Trustseal.eNamad.ir/logo.aspx?id=204904&amp;Code=ipcUNmaUZGludD970sgm" alt="" style="cursor:pointer" id="ipcUNmaUZGludD970sgm"></a>
 
-<img referrerpolicy="origin" id = 'jxlzsizpjzpejxlznbqergvj' style = 'cursor:pointer' onclick = 'window.open("https://logo.samandehi.ir/Verify.aspx?id=197123&p=rfthpfvljyoerfthuiwkxlao", "Popup","toolbar=no, scrollbars=no, location=no, statusbar=no, menubar=no, resizable=0, width=450, height=630, top=30")' alt = 'logo-samandehi' src = 'https://logo.samandehi.ir/logo.aspx?id=197123&p=nbpdbsiyyndtnbpdodrfqfti' />
+<img referrerpolicy="origin" id="jxlzsizpjzpejxlznbqergvj" style="cursor:pointer" onclick='window.open("https://logo.samandehi.ir/Verify.aspx?id=197123&p=rfthpfvljyoerfthuiwkxlao", "Popup","toolbar=no, scrollbars=no, location=no, statusbar=no, menubar=no, resizable=0, width=450, height=630, top=30")' alt ="logo-samandehi" src ="https://logo.samandehi.ir/logo.aspx?id=197123&p=nbpdbsiyyndtnbpdodrfqfti">
 
 
 <a href="https://tehran.irannsr.org/fa/page/104366-%D9%85%D8%B4%D8%A7%D9%87%D8%AF%D9%87-%D8%A7%D8%B9%D8%B6%D8%A7.html?ctp_id=1086&amp;id=25122" target="_blank"><img width="150" src="/images/irannsr.png">
