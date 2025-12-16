e45ef40817f54811c6bea7f93aeafa582b315f8d
Moslem <monfaredman@outlook.com>
2025-12-16 13:13:48 +0330

    chore: update HTML files and script references for improved performance and functionality across various pages

---
+++ dist/Upera/_nuxt/4ec98b0.js
@@ -0,0 +1,134 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[88],{
+
+/***/ 780:
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by extract-css-chunks-webpack-plugin
+module.exports = {};
+
+/***/ }),
+
+/***/ 830:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var _node_modules_extract_css_chunks_webpack_plugin_dist_loader_js_ref_4_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_4_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_dist_cjs_js_ref_4_oneOf_1_2_node_modules_nuxt_components_dist_loader_js_ref_1_0_node_modules_vue_loader_lib_index_js_vue_loader_options_node_modules_string_replace_loader_index_js_ref_13_SeasonEpisodesSkeleton_vue_vue_type_style_index_0_id_600f1162_prod_scoped_true_lang_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(780);
+/* harmony import */ var _node_modules_extract_css_chunks_webpack_plugin_dist_loader_js_ref_4_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_4_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_dist_cjs_js_ref_4_oneOf_1_2_node_modules_nuxt_components_dist_loader_js_ref_1_0_node_modules_vue_loader_lib_index_js_vue_loader_options_node_modules_string_replace_loader_index_js_ref_13_SeasonEpisodesSkeleton_vue_vue_type_style_index_0_id_600f1162_prod_scoped_true_lang_css__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_extract_css_chunks_webpack_plugin_dist_loader_js_ref_4_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_4_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_dist_cjs_js_ref_4_oneOf_1_2_node_modules_nuxt_components_dist_loader_js_ref_1_0_node_modules_vue_loader_lib_index_js_vue_loader_options_node_modules_string_replace_loader_index_js_ref_13_SeasonEpisodesSkeleton_vue_vue_type_style_index_0_id_600f1162_prod_scoped_true_lang_css__WEBPACK_IMPORTED_MODULE_0__);
+/* unused harmony reexport * */
+
+
+/***/ }),
+
+/***/ 923:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// CONCATENATED MODULE: ./node_modules/babel-loader/lib??ref--3-0!./node_modules/vue-loader/lib/loaders/templateLoader.js??ref--6!./node_modules/@nuxt/components/dist/loader.js??ref--1-0!./node_modules/vue-loader/lib??vue-loader-options!./node_modules/string-replace-loader??ref--13!./components/item/skeletons/SeasonEpisodesSkeleton.vue?vue&type=template&id=600f1162&scoped=true
+var render = function render() {
+  var _vm = this,
+    _c = _vm._self._c;
+  return _c('section', {
+    staticClass: "horizontal-list-container item mt-lg-2 pt-3",
+    attrs: {
+      "id": "watching"
+    }
+  }, [_vm._m(0), _vm._v(" "), _vm._m(1), _vm._v(" "), _c('div', {
+    staticClass: "container-fluid"
+  }, [_c('div', {
+    staticClass: "row"
+  }, _vm._l(8, function (i) {
+    return _c('div', {
+      key: i,
+      staticClass: "col-lg-3 col-md-6 col-6 mb-3"
+    }, [_vm._m(2, true)]);
+  }), 0)])]);
+};
+var staticRenderFns = [function () {
+  var _vm = this,
+    _c = _vm._self._c;
+  return _c('div', {
+    staticClass: "container-fluid mb-3"
+  }, [_c('div', {
+    staticClass: "row align-items-center"
+  }, [_c('div', {
+    staticClass: "col-auto"
+  }, [_c('div', {
+    staticClass: "skeleton-dropdown"
+  })]), _vm._v(" "), _c('div', {
+    staticClass: "col-auto"
+  }, [_c('div', {
+    staticClass: "skeleton-dropdown"
+  })])])]);
+}, function () {
+  var _vm = this,
+    _c = _vm._self._c;
+  return _c('div', {
+    staticClass: "container-fluid mb-4"
+  }, [_c('div', {
+    staticClass: "row"
+  }, [_c('div', {
+    staticClass: "col-12"
+  }, [_c('div', {
+    staticClass: "skeleton-description mb-2"
+  }), _vm._v(" "), _c('div', {
+    staticClass: "skeleton-description",
+    staticStyle: {
+      "width": "70%"
+    }
+  })])])]);
+}, function () {
+  var _vm = this,
+    _c = _vm._self._c;
+  return _c('div', {
+    staticClass: "episode-card-skeleton"
+  }, [_c('div', {
+    staticClass: "skeleton-episode-image"
+  }), _vm._v(" "), _c('div', {
+    staticClass: "skeleton-episode-title mt-2"
+  }), _vm._v(" "), _c('div', {
+    staticClass: "skeleton-episode-meta mt-2"
+  })]);
+}];
+
+// CONCATENATED MODULE: ./components/item/skeletons/SeasonEpisodesSkeleton.vue?vue&type=template&id=600f1162&scoped=true
+
+// CONCATENATED MODULE: ./node_modules/babel-loader/lib??ref--3-0!./node_modules/@nuxt/components/dist/loader.js??ref--1-0!./node_modules/vue-loader/lib??vue-loader-options!./node_modules/string-replace-loader??ref--13!./components/item/skeletons/SeasonEpisodesSkeleton.vue?vue&type=script&lang=js
+/* harmony default export */ var SeasonEpisodesSkeletonvue_type_script_lang_js = ({
+  name: 'SeasonEpisodesSkeleton'
+});
+// CONCATENATED MODULE: ./components/item/skeletons/SeasonEpisodesSkeleton.vue?vue&type=script&lang=js
+ /* harmony default export */ var skeletons_SeasonEpisodesSkeletonvue_type_script_lang_js = (SeasonEpisodesSkeletonvue_type_script_lang_js); 
+// EXTERNAL MODULE: ./components/item/skeletons/SeasonEpisodesSkeleton.vue?vue&type=style&index=0&id=600f1162&prod&scoped=true&lang=css
+var SeasonEpisodesSkeletonvue_type_style_index_0_id_600f1162_prod_scoped_true_lang_css = __webpack_require__(830);
+
+// EXTERNAL MODULE: ./node_modules/vue-loader/lib/runtime/componentNormalizer.js
+var componentNormalizer = __webpack_require__(32);
+
+// CONCATENATED MODULE: ./components/item/skeletons/SeasonEpisodesSkeleton.vue
+
+
+
+
+
+
+/* normalize component */
+
+var component = Object(componentNormalizer["a" /* default */])(
+  skeletons_SeasonEpisodesSkeletonvue_type_script_lang_js,
+  render,
+  staticRenderFns,
+  false,
+  null,
+  "600f1162",
+  null
+  
+)
+
+/* harmony default export */ var SeasonEpisodesSkeleton = __webpack_exports__["default"] = (component.exports);
+
+/***/ })
+
+}]);
---
+++ dist/Upera/_nuxt/9683a6f.js
@@ -0,0 +1,197 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[129],{
+
+/***/ 1070:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// CONCATENATED MODULE: ./node_modules/babel-loader/lib??ref--3-0!./node_modules/vue-loader/lib/loaders/templateLoader.js??ref--6!./node_modules/@nuxt/components/dist/loader.js??ref--1-0!./node_modules/vue-loader/lib??vue-loader-options!./node_modules/string-replace-loader??ref--13!./pages/profile/about-contact.vue?vue&type=template&id=0a821b4d
+var render = function render() {
+  var _vm = this,
+    _c = _vm._self._c;
+  return _c('div', {
+    staticClass: "container-fluid",
+    attrs: {
+      "id": "checkcontainer"
+    }
+  }, [_c('div', {
+    staticClass: "container pt-5 pt-lg-5 pt-md-5 mb-5 pt-1 faq-page"
+  }, [_c('section', {
+    attrs: {
+      "id": "banner"
+    }
+  }, [_c('div', {
+    staticClass: "container"
+  }, [_c('header', {
+    staticClass: "headline mt-5"
+  }, [_c('h5', {
+    staticClass: "title font-weight-bold"
+  }, [_vm._v("\n            " + _vm._s(_vm.$t('footer.aboutus')) + "\n          ")])])])]), _vm._v(" "), _c('br'), _c('br'), _vm._v(" "), _vm.checkuser.about && _vm.checkuser.about.length > 5 ? _c('div', [_c('div', {
+    directives: [{
+      name: "lazy-load",
+      rawName: "v-lazy-load",
+      value: _vm.checkuser.about,
+      expression: "checkuser.about"
+    }],
+    staticClass: "body text-justify"
+  })]) : _c('div', [_c('div', {
+    directives: [{
+      name: "lazy-load",
+      rawName: "v-lazy-load",
+      value: _vm.data,
+      expression: "data"
+    }],
+    staticClass: "body text-justify"
+  })]), _vm._v(" "), _vm._m(0), _vm._v(" "), _c('br'), _c('br'), _vm._v(" "), _c('div', [_c('b-table', {
+    attrs: {
+      "dark": "",
+      "items": _vm.items
+    }
+  })], 1)])]);
+};
+var staticRenderFns = [function () {
+  var _vm = this,
+    _c = _vm._self._c;
+  return _c('section', {
+    attrs: {
+      "id": "banner"
+    }
+  }, [_c('div', {
+    staticClass: "container"
+  }, [_c('header', {
+    staticClass: "headline mt-5"
+  }, [_c('h5', {
+    staticClass: "title font-weight-bold"
+  }, [_vm._v("سیستم درجه‌بندی سنی")])])])]);
+}];
+
+// CONCATENATED MODULE: ./pages/profile/about-contact.vue?vue&type=template&id=0a821b4d
+
+// EXTERNAL MODULE: ./node_modules/@babel/runtime/helpers/esm/asyncToGenerator.js
+var asyncToGenerator = __webpack_require__(27);
+
+// EXTERNAL MODULE: ./node_modules/regenerator-runtime/runtime.js
+var runtime = __webpack_require__(64);
+
+// CONCATENATED MODULE: ./node_modules/babel-loader/lib??ref--3-0!./node_modules/@nuxt/components/dist/loader.js??ref--1-0!./node_modules/vue-loader/lib??vue-loader-options!./node_modules/string-replace-loader??ref--13!./pages/profile/about-contact.vue?vue&type=script&lang=js
+
+
+/* harmony default export */ var about_contactvue_type_script_lang_js = ({
+  asyncData: function asyncData(context) {
+    return Object(asyncToGenerator["a" /* default */])(/*#__PURE__*/regeneratorRuntime.mark(function _callee() {
+      var res;
+      return regeneratorRuntime.wrap(function (_context) {
+        while (1) switch (_context.prev = _context.next) {
+          case 0:
+            _context.next = 1;
+            return context.app.$axios.get('/get/app/details');
+          case 1:
+            res = _context.sent;
+            if (!(context.app.i18n.locale == 'fa')) {
+              _context.next = 2;
+              break;
+            }
+            return _context.abrupt("return", {
+              data: res.data.data.about
+            });
+          case 2:
+            return _context.abrupt("return", {
+              data: res.data.data.about_en
+            });
+          case 3:
+          case "end":
+            return _context.stop();
+        }
+      }, _callee);
+    }))();
+  },
+  data: function data() {
+    return {
+      data: '',
+      items: [{
+        'درجه‌بندی/نشان': 'G',
+        معنی: 'جی – تماشاگران عمومی',
+        توضیحات: 'مناسب تمامی سنین. هیچ چیزی باعث آزار والدین برای تماشای کودکان نمی‌شود.'
+      }, {
+        'درجه‌بندی/نشان': 'PG',
+        معنی: 'پی‌جی – سرپرستی والدین پیشنهاد می‌شود',
+        توضیحات: 'برخی از مواد ممکن است برای کودکان مناسب نباشد. راهنمایی و سرپرستی از سوی والدین تقاضا می‌شود. ممکن است حاوی مطالبی باشد که والدین برای فرزندان خردسال خود مناسب ندانند.'
+      }, {
+        'درجه‌بندی/نشان': 'PG-13',
+        معنی: 'پی‌جی-۱۳ – تذکر قاطع به والدین',
+        توضیحات: 'برخی از صحنه ها برای افراد زیر ۱۳ سال نامناسب است. از والدین درخواست می‌شود احتیاط کنند.'
+      }, {
+        'درجه‌بندی/نشان': 'R',
+        معنی: 'آر – محدود',
+        توضیحات: 'زیر ۱۸ سال به همراهی والدین یا سرپرست نیاز دارد. حاوی تعدادی محتوای بزرگسالانه است.'
+      }, {
+        'درجه‌بندی/نشان': 'X',
+        معنی: 'فقط بزرگسالان',
+        توضیحات: 'هیچ فرد کمتر از ۱۸ سال نباید این فیلم ها را ببینند. فقط بزرگسالان. کودکان اجازهٔ ورود ندارند.'
+      }]
+    };
+  },
+  head: function head() {
+    return {
+      title: this.$t('footer.aboutus')
+    };
+  }
+});
+// CONCATENATED MODULE: ./pages/profile/about-contact.vue?vue&type=script&lang=js
+ /* harmony default export */ var profile_about_contactvue_type_script_lang_js = (about_contactvue_type_script_lang_js); 
+// EXTERNAL MODULE: ./pages/profile/about-contact.vue?vue&type=style&index=0&id=0a821b4d&prod&lang=css
+var about_contactvue_type_style_index_0_id_0a821b4d_prod_lang_css = __webpack_require__(954);
+
+// EXTERNAL MODULE: ./node_modules/vue-loader/lib/runtime/componentNormalizer.js
+var componentNormalizer = __webpack_require__(32);
+
+// CONCATENATED MODULE: ./pages/profile/about-contact.vue
+
+
+
+
+
+
+/* normalize component */
+
+var component = Object(componentNormalizer["a" /* default */])(
+  profile_about_contactvue_type_script_lang_js,
+  render,
+  staticRenderFns,
+  false,
+  null,
+  null,
+  null
+  
+)
+
+/* harmony default export */ var about_contact = __webpack_exports__["default"] = (component.exports);
+
+/* nuxt-component-imports */
+installComponents(component, {Header: __webpack_require__(124).default})
+
+
+/***/ }),
+
+/***/ 866:
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by extract-css-chunks-webpack-plugin
+module.exports = {};
+
+/***/ }),
+
+/***/ 954:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var _node_modules_extract_css_chunks_webpack_plugin_dist_loader_js_ref_4_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_4_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_dist_cjs_js_ref_4_oneOf_1_2_node_modules_nuxt_components_dist_loader_js_ref_1_0_node_modules_vue_loader_lib_index_js_vue_loader_options_node_modules_string_replace_loader_index_js_ref_13_about_contact_vue_vue_type_style_index_0_id_0a821b4d_prod_lang_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(866);
+/* harmony import */ var _node_modules_extract_css_chunks_webpack_plugin_dist_loader_js_ref_4_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_4_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_dist_cjs_js_ref_4_oneOf_1_2_node_modules_nuxt_components_dist_loader_js_ref_1_0_node_modules_vue_loader_lib_index_js_vue_loader_options_node_modules_string_replace_loader_index_js_ref_13_about_contact_vue_vue_type_style_index_0_id_0a821b4d_prod_lang_css__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_extract_css_chunks_webpack_plugin_dist_loader_js_ref_4_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_4_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_dist_cjs_js_ref_4_oneOf_1_2_node_modules_nuxt_components_dist_loader_js_ref_1_0_node_modules_vue_loader_lib_index_js_vue_loader_options_node_modules_string_replace_loader_index_js_ref_13_about_contact_vue_vue_type_style_index_0_id_0a821b4d_prod_lang_css__WEBPACK_IMPORTED_MODULE_0__);
+/* unused harmony reexport * */
+
+
+/***/ })
+
+}]);
